b00537405b54baab33b73931101ba703ebf50368
dinushchathurya <dinushchathurya21@gmail.com>
2022-01-01 20:41:15 +0530

    feat: Create function to create new ToDo item

---
+++ src/addToDo.js
@@ -0,0 +1,32 @@
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+
+const addToDo = async (event) => {
+
+    const dynamoddb = new AWS.DynamoDB.DocumentClient();
+
+    const { todo } = JSON.parse(event.body);
+    const createdAt = new Date();
+    const id = v4();
+    
+    const newToDo = {
+        id,
+        todo,
+        createdAt,
+        completed: false
+    }
+    
+    await dynamoddb.put({
+        TableName: "ToDoTable",
+        Item: newToDo
+    })
+
+    return {
+        statusCode: 200,
+        body: JSON.stringify(newToDo ),
+    };
+};
+
+module.exports = {
+  handler: addToDo
+}
